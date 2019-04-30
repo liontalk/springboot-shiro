ae4c91decebad325c9c62a31159af9866d9d4eb7
liontalk <liontalk@163.com>
2019-04-30 17:19:54 +0800

    增加Redis环境配置

--- src/main/java/cn/liontalk/springbootshiro/config/RedisConfig.java
+++ src/main/java/cn/liontalk/springbootshiro/config/RedisConfig.java
@@ -3,13 +3,17 @@ package cn.liontalk.springbootshiro.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @author Administrator
@@ -19,6 +23,55 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @Configuration
 public class RedisConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.block-when-exhausted}")
+    private boolean  blockWhenExhausted;
+
+    @Value("${spring.redis.jedis.pool.max-active}")
+    private int maxActive;
+
+
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private int maxWait;
+
+
+    @Bean
+    public JedisPool redisPoolFactory()  throws Exception{
+        logger.info("JedisPool注入成功！！");
+        logger.info("redis地址：" + host + ":" + port);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
+        jedisPoolConfig.setBlockWhenExhausted(blockWhenExhausted);
+        // 是否启用pool的jmx管理功能, 默认true
+        jedisPoolConfig.setJmxEnabled(true);
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
+        return jedisPool;
+    }
+
+
+
 
     /**
      *  定义 StringRedisTemplate ，指定序列化和反序列化的处理类
---
+++ src/main/java/cn/liontalk/springbootshiro/util/RedisUtil.java
@@ -0,0 +1,10 @@
+package cn.liontalk.springbootshiro.util;
+
+/**
+ * @author Administrator
+ * @projectName springboot-shiro
+ * @description:
+ * @date 2019/4/30 17:17
+ */
+public class RedisUtil {
+}
