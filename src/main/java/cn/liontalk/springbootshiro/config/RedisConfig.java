296704fb1609960d09b3867f56e796acf4b28e51
liontalk <liontalk@163.com>
2019-05-05 23:43:51 +0800

    重置密码修改

--- src/main/java/cn/liontalk/springbootshiro/config/RedisConfig.java
+++ src/main/java/cn/liontalk/springbootshiro/config/RedisConfig.java
@@ -12,8 +12,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @author Administrator
@@ -44,8 +42,8 @@ public class RedisConfig {
     @Value("${spring.redis.password}")
     private String password;
 
-    @Value("${spring.redis.block-when-exhausted}")
-    private boolean  blockWhenExhausted;
+//    @Value("${spring.redis.block-when-exhausted}")
+//    private boolean  blockWhenExhausted;
 
     @Value("${spring.redis.jedis.pool.max-active}")
     private int maxActive;
@@ -55,20 +53,20 @@ public class RedisConfig {
     private int maxWait;
 
 
-    @Bean
-    public JedisPool redisPoolFactory()  throws Exception{
-        logger.info("JedisPool注入成功！！");
-        logger.info("redis地址：" + host + ":" + port);
-        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setMaxIdle(maxIdle);
-        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
-        // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
-        jedisPoolConfig.setBlockWhenExhausted(blockWhenExhausted);
-        // 是否启用pool的jmx管理功能, 默认true
-        jedisPoolConfig.setJmxEnabled(true);
-        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
-        return jedisPool;
-    }
+//    @Bean
+//    public JedisPool redisPoolFactory()  throws Exception{
+//        logger.info("JedisPool注入成功！！");
+//        logger.info("redis地址：" + host + ":" + port);
+//        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+//        jedisPoolConfig.setMaxIdle(maxIdle);
+//        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+//        // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
+//        jedisPoolConfig.setBlockWhenExhausted(blockWhenExhausted);
+//        // 是否启用pool的jmx管理功能, 默认true
+//        jedisPoolConfig.setJmxEnabled(true);
+//        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
+//        return jedisPool;
+//    }
 
 
 
