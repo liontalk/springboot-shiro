aaec28d1aa7505952f5fe5f5e255bac19c836d8b
liontalk <liontalk@163.com>
2019-05-09 22:05:39 +0800

    选中用户已有的角色

--- src/main/resources/static/js/appjs/sys/user/edit.js
+++ src/main/resources/static/js/appjs/sys/user/edit.js
@@ -1,124 +1,130 @@
 // 以下为官方示例
-$().ready(function() {
-	validateRule();
+$().ready(function () {
+    validateRule();
     setCheckedRoles();
 });
 
 $.validator.setDefaults({
-	submitHandler : function() {
-		update();
-	}
+    submitHandler: function () {
+        update();
+    }
 });
 function update() {
-	$("#roleEntityList").val(getCheckedRoles());
-	$.ajax({
-		cache : true,
-		type : "POST",
-		url : "/manager/update",
-		data : $('#signupForm').serialize(),// 你的formid
-		async : false,
-		error : function(request) {
-			console.log("Connection error");
-		},
-		success : function(data) {
-			if (data.code == 0) {
-				parent.layer.msg("操作成功!");
-				parent.reLoad();
-				var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
-				parent.layer.close(index);
+    $("#roleEntityList").val(getCheckedRoles());
+    $.ajax({
+        cache: true,
+        type: "POST",
+        url: "/manager/update",
+        data: $('#signupForm').serialize(),// 你的formid
+        async: false,
+        error: function (request) {
+            console.log("Connection error");
+        },
+        success: function (data) {
+            if (data.code == 0) {
+                parent.layer.msg("操作成功!");
+                parent.reLoad();
+                var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
+                parent.layer.close(index);
 
-			} else {
-				parent.layer.msg(data.msg);
-			}
+            } else {
+                parent.layer.msg(data.msg);
+            }
 
-		}
-	});
+        }
+    });
 
 }
 function getCheckedRoles() {
-	var adIds = "";
-	$("input:checkbox[name=role]:checked").each(function(i) {
-		if (0 == i) {
-			adIds = $(this).val();
-		} else {
-			adIds += ("," + $(this).val());
-		}
-	});
-	return adIds;
+    var adIds = "";
+    $("input:checkbox[name=role]:checked").each(function (i) {
+        if (0 == i) {
+            adIds = $(this).val();
+        } else {
+            adIds += ("," + $(this).val());
+        }
+    });
+    return adIds;
 }
 function setCheckedRoles() {
-	var roleIds = $("#roleEntityList").val();
-	var adIds = "";
-	$("input:checkbox[name=role]:checked").each(function(i) {
-		if (0 == i) {
-			adIds = $(this).val();
-		} else {
-			adIds += ("," + $(this).val());
-		}
-	});
-	return adIds;
-}
-function validateRule() {
-	var icon = "<i class='fa fa-times-circle'></i> ";
-	$("#signupForm").validate({
-		rules : {
-			name : {
-				required : true
-			},
-			username : {
-				required : true,
-				minlength : 2
-			},
-			password : {
-				required : true,
-				minlength : 6
-			},
-			confirm_password : {
-				required : true,
-				minlength : 6,
-				equalTo : "#password"
-			},
-			email : {
-				required : true,
-				email : true
-			},
-			topic : {
-				required : "#newsletter:checked",
-				minlength : 2
-			},
-			agree : "required"
-		},
-		messages : {
+    var roleEntityList = $("#roleEntityList").val();
+    var result = roleEntityList.substring(1, roleEntityList.length - 1);
+    var ids = result.split(",");
+    var roleList = $("#roleList").val();
+    var listcheck = document.getElementsByName("role");//获取checkbox列表
+    for(var i=0;i<listcheck.length;i++){
+        for (var j in ids) {
+            if (listcheck[i].value == ids[j]) {
+                listcheck[i].checked = true;
+            }
+        }
+    }
 
-			name : {
-				required : icon + "请输入姓名"
-			},
-			username : {
-				required : icon + "请输入您的用户名",
-				minlength : icon + "用户名必须两个字符以上"
-			},
-			password : {
-				required : icon + "请输入您的密码",
-				minlength : icon + "密码必须6个字符以上"
-			},
-			confirm_password : {
-				required : icon + "请再次输入密码",
-				minlength : icon + "密码必须6个字符以上",
-				equalTo : icon + "两次输入的密码不一致"
-			},
-			email : icon + "请输入您的E-mail",
-		}
-	})
+
 }
-var openDept = function(){
-    layer.open({
-        type:2,
-        title:"选择部门",
-        area : [ '300px', '450px' ],
-        content:"/department/treeView"
-    })
-}
-function loadDept( deptId,deptName){
-	$("#deptId").val(deptId);
-	$("#deptName").val(deptName);
-}
+    function validateRule() {
+        var icon = "<i class='fa fa-times-circle'></i> ";
+        $("#signupForm").validate({
+            rules: {
+                name: {
+                    required: true
+                },
+                username: {
+                    required: true,
+                    minlength: 2
+                },
+                password: {
+                    required: true,
+                    minlength: 6
+                },
+                confirm_password: {
+                    required: true,
+                    minlength: 6,
+                    equalTo: "#password"
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                topic: {
+                    required: "#newsletter:checked",
+                    minlength: 2
+                },
+                agree: "required"
+            },
+            messages: {
+
+                name: {
+                    required: icon + "请输入姓名"
+                },
+                username: {
+                    required: icon + "请输入您的用户名",
+                    minlength: icon + "用户名必须两个字符以上"
+                },
+                password: {
+                    required: icon + "请输入您的密码",
+                    minlength: icon + "密码必须6个字符以上"
+                },
+                confirm_password: {
+                    required: icon + "请再次输入密码",
+                    minlength: icon + "密码必须6个字符以上",
+                    equalTo: icon + "两次输入的密码不一致"
+                },
+                email: icon + "请输入您的E-mail",
+            }
+        })
+    }
+
+    var openDept = function () {
+        layer.open({
+            type: 2,
+            title: "选择部门",
+            area: ['300px', '450px'],
+            content: "/department/treeView"
+        })
+    }
+
+    function loadDept(deptId, deptName) {
+        $("#deptId").val(deptId);
+        $("#deptName").val(deptName);
+    }
